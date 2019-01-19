0a86a2000fbbd21a2868cf6e1a86cca99e9b15c1
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2019-01-19 01:41:43 +0000

    Fraction Interface, FractionImpl class and Main class updated

---
+++ cwork/src/fraction/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import fraction.FractionImpl;
+
+public class Main {
+
+
+    public static void main( String[] args )
+    {
+
+        // Create two new Fraction objects for testing the functions
+        FractionImpl fracA = new FractionImpl("50/100");
+        FractionImpl fracB = new FractionImpl(25, 100);
+//        System.out.println(fracA.add(fracB));
+//        System.out.println(fracA.subtract(fracB));
+//        System.out.println(fracA.multiply(fracB));
+//        System.out.println(fracA.divide(fracB));
+//        System.out.println(fracA.abs());
+//        System.out.println(fracA.negate());
+//        System.out.println(fracA.equals(fracB));
+//        System.out.println(fracA.inverse());
+//        System.out.println(fracA.compareTo(fracB));
+        System.out.println(fracA.toString());
+
+
+    }
+
+
+}
